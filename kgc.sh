da1dd0ad6797386cf136d2b84ef9833cd74ffa08
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2017-02-16 09:53:54 +0100

    Running script

---
+++ kgc.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+red='\033[0;31m'			# Red
+nc='\033[0m'				# No color
+re='^[0-9]+$'				# Regular expression to detect natural numbers
+
+usage() { echo -e "Usage: $0 -i <filename> [-c]\n-i\tInput filename\n-c\tEnable CSV output (optional)" 1>&2; exit 1; }
+
+while getopts ":i:s:c" o; do
+	case "${o}" in
+	i)
+		i=${OPTARG}
+		if [ ! -f "$i" ]
+		then
+			echo -e "${red}Input file \"$i\" not found!${nc}\n" 1>&2
+			usage
+		fi
+		;;
+	c)
+		c=1
+		;;
+	\?)
+		echo -e "${red}-$OPTARG is not a valid option!${nc}\n" 1>&2
+		usage
+		;;
+	esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${i}" ]
+then
+	echo -e "${red}Missing input file!${nc}\n" 1>&2
+	usage
+fi
+
+N=`sed '1q;d' $i`	# Number of agents
+K=`sed '2q;d' $i`	# Maximum cardinality
+
+tmp=`mktemp`
+echo "#define N $N" > $tmp
+echo "#define K $K" >> $tmp
+
+if [ ! -z "${c}" ]
+then
+	echo "#define CSV" >> $tmp
+fi
+
+if [ ! -f instance.h ]
+then
+	mv $tmp "instance.h"
+else
+	md5a=`md5sum instance.h | cut -d\  -f 1`
+	md5b=`md5sum $tmp | cut -d\  -f 1`
+
+	if [ $md5a != $md5b ]
+	then
+		mv $tmp "instance.h"
+	else
+		rm $tmp
+	fi
+fi
+
+make -j
+if [[ $? == 0 ]]
+then
+	bin=$0
+	bin=${bin%???}
+	$bin $i $s
+fi
